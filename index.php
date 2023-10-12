6ec4519626ff22edf1fb0af60149a2053fe0a86f
robsn-se <rubikserobyan17@mail.ru>
2023-10-12 19:12:34 +0300

    json array

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <?php
 require_once "config.php";
 require_once "functions.php";
+require_once "schedule";
 
 try {
     $phpInput = json_decode(file_get_contents("php://input"), true);
+
+//    $schedule = json_decode(file_get_contents("schedule"), true);
+
+
+    if (@$_GET["text"]) {
+        $params["chat_id"] = ADMIN_ID;
+        $params["text"] = @$_GET["text"];
+        telegramAPIRequest("sendMessage", $params);
+    }
 
     if (!isset($phpInput["update_id"])) {
         die();
     }
 
-    if (@$phpInput["message"]) {
-        $params["chat_id"] = $phpInput["message"]["chat"]["id"];
-        $params["text"] = 'I got phrase!!!: ' . mb_strtolower($phpInput["message"]["text"]);
-//        $params["reply_markup"] => json_encode("inline_")
-        $params["reply_markup"] = createInlineButtons(OK_BUTTON);
-        telegramAPIRequest("sendMessage", $params);
-    }
+
+
+//    if (@$phpInput["message"]) {
+//        $params["chat_id"] = $phpInput["message"]["chat"]["id"];
+//        $params["text"] = 'I got phrase!!!: ' . mb_strtolower($phpInput["message"]["text"]);
+////        $params["reply_markup"] => json_encode("inline_")
+//        $params["reply_markup"] = createInlineButtons(OK_BUTTON);
+//        telegramAPIRequest("sendMessage", $params);
+//    }
 }
 
 catch (Throwable $e) {
