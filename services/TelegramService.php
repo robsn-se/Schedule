8eb656b7fbd60d498de0299ec37a61d436ed3fa8
robsn-se <rubikserobyan17@mail.ru>
2024-11-22 21:58:54 +0300

    response to the command /start

--- services/TelegramService.php
+++ services/TelegramService.php
@@ -2,23 +2,52 @@
 
 namespace services;
 
+use bot\BotAPI;
 use core\Log;
 use exceptions\SystemFailure;
 
 class TelegramService
 {
-    public static function hookEntrePoint(array $requestBody): bool{
+    public static function hookEntrePoint(array $requestBody): bool {
         if (isset($requestBody["callback_query"])) {
             Log::add($requestBody, "callback_query");
         }
         elseif (isset($requestBody["message"])) {
             $params["chat_id"] = $requestBody["message"]["chat"]["id"];
             $request = mb_strtolower($requestBody["message"]["text"]);
+            $entities = self::getEntities($requestBody["message"]);
+            if (isset($entities["bot_command"])) {
+                $params["text"] = print_r($entities["bot_command"], true);
+                BotAPI::sendRequest("sendMessage", $params);
+            }
             Log::add($requestBody, "message");
         }
         else{
             throw new SystemFailure("unexpected hook request");
         }
         return true;
+    }
+
+    public static function getEntities(array $message): array {
+        $entities = [];
+        if (
+            trim($message["text"])
+            && isset($message["entities"])
+            && is_array($message["entities"])
+        ) {
+            foreach ($message["entities"] as $entity) {
+                if (isset($entity["type"])) {
+                    $command = substr($message["text"], $entity["offset"], $entity["length"]);
+                    if (!isset($entities[$entity["type"]])) {
+                        $entities[$entity["type"]] = [0 => $command];
+                    }
+                    else {
+                        $entities[$entity["type"]][] = $command;
+                    }
+                }
+
+            }
+        }
+        return $entities;
     }
 }
